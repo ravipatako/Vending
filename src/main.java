6ca8d0e78c6509bb26341201812be0e97d3ee92a
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2020-05-11 19:11:44 -0400

    Should Be working
    Fixed Hopefully if it pushes

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+//Created by Ravi Pathak
+
+/*
+ *Hello so if you are reading this then that means you found out about my vending machine
+ * This program was creating to be able to allow me to just input my collections and expenses
+ * to figure out my monthly profits. Enjoy the work!!
+ */
+
+
+public class main{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in); //creating menu scanner
+
+        //right here is the main menu words
+        System.out.println("Welcome to the main menu \n");
+        System.out.println("Select from the menu: ") ;
+        System.out.println("1 - Input Collection");
+        System.out.println("2 - Input Inventory");
+        System.out.println("3 - Get Report");
+
+        int menu = sc.nextInt(); //creating the variable for the menu selection
+
+
+
+    }
+}
