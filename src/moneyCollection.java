f465457d62f4a334bcc8c9d09d11fdea70680626
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2020-12-08 09:55:33 -0500

    Pushing from IDE

--- src/moneyCollection.java
+++ src/moneyCollection.java
@@ -1,6 +1,10 @@
 //This is now a new class, I thought it would be good to have a method but turns out a class might just work better
 public class moneyCollection {
     public static int collectionAmount(){
+        int date;
+        double moneyCollected;
+
+
         return 0;
     }
 
