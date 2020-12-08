e06a645d48f5e138ff057ac1f5192a75c90d6293
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2020-12-08 10:18:17 -0500

    More updates
    Update

--- src/moneyCollection.java
+++ src/moneyCollection.java
@@ -3,23 +3,23 @@ import java.util.Date;
 
 //This is now a new class, I thought it would be good to have a method but turns out a class might just work better
 public class moneyCollection {
-    public static void collectionAmount(){
-
-
+    public static double collectionAmount(){
+        //Creating the date object
         Date todayDate = new Date();
-        System.out.println(todayDate);
 
+        //Creating the scanner for the amount
         Scanner newScanner = new Scanner(System.in);
         System.out.println("Please enter the amount: ");
         double moneyCollected = newScanner.nextDouble();
-        System.out.println(moneyCollected);
 
+        //Printing out everything that was collected
+        System.out.println("Today " + todayDate + " You collected: " + moneyCollected);
 
+        //returning the money that was collected at set date to be used elsewhere
+        return moneyCollected;
     }
-
     public static void totalCollected(){
 
     }
-
 
 }
